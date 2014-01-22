80f970db2741cca1871c370298879fc80ca90bb8
vickyonit <vignesh.theemperor@gmail.com>
2014-01-23 01:46:42 +0530

    added devise to project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rshare::Application.config.secret_key_base = 'aac8140f8558546607c5ab3b28633d812cd0b14a4178d684b5c65ce90d246728efdd8e50a8266faca228355a6ca325ca537d3ea5ea6cf9bcf2babd6bf3724c70'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rshare::Application.config.session_store :cookie_store, key: '_rshare_session'
